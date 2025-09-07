f2c8056d29a795570ee98ad5d1acbe40c6b663a5
= <=>
2025-09-07 18:06:40 +0530

    feat: Escape seq and Type

---
+++ EscapeSequence.py
@@ -0,0 +1,24 @@
+# tab spaced string
+tab_spaced_str = "This\tis\ta\ttab\tspaced\tstring"
+print(tab_spaced_str) # This	is	a	tab	spaced	string
+
+# new line string
+tab_spaced_str = "This\nis\na\nnew line\nspaced\nstring"
+print(tab_spaced_str)   # This
+                        # is
+                        # a
+                        # new line
+                        # spaced
+                        # string
+
+# escape sequence for single quote within the double quote
+escape_seq_double_quotes = '"There is a \'single quote\' within this double quote here"'
+print(escape_seq_double_quotes)
+
+# escape sequence for single quote within the double quote
+escape_seq_double_quotes = "\'There entire string is within single quote\'"
+print(escape_seq_double_quotes)
+
+# escape sequence for \ character within the string
+slash_char = "This string contains a slash character here \\"
+print(slash_char)
---
+++ Type.py
@@ -0,0 +1,19 @@
+# Type Operator
+
+int_val = 50
+float_val = 3.14
+bool_val = True
+
+print(type(int_val)) # <class 'int'>
+print(type(float_val)) # <class 'float'>
+print(type(bool_val)) # <class 'bool'>
+
+# Str Operator
+
+int_val = str(50)
+float_val = str(3.14)
+bool_val = str(True)
+
+print(type(int_val)) # <class 'str'>
+print(type(float_val)) # <class 'str'>
+print(type(bool_val)) # <class 'str'>
